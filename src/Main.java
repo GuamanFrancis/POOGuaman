ed5f0b736a54e945c9410452630f651d66cbf1e2
POO <francisguaman909@gmail.com>
2024-05-23 07:57:35 -0500

    pratica

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        hija hija1 = new hija("daniela", 20);
+
+
+        hija1.datos();
+    }
+}
---
+++ src/hija.java
@@ -0,0 +1,24 @@
+public class hija {
+
+    String nombre;
+    int edad;
+
+    public hija(String nombre,  int edad) {
+
+        this.nombre = nombre;
+        this.edad = edad;
+
+    }
+
+
+    public void datos(){
+        System.out.println("El nombre es "+nombre+" y su edad es " +edad);
+
+
+    }
+
+
+
+
+
+}
